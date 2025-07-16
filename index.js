e7b44677253edf31a2808889931c656066d789e5
Regendra Suman <regendrasuman2017@gmail.com>
2025-07-16 23:01:43 +0530

    DD

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+const app = express();
+
+// Handle undefined routes (404 Not Found)
+// Start the server
+const startServer = async () => {
+    try {
+        const port = 3000;
+        app.listen(port, () => {
+            console.log(` Server running at: http://localhost:${port}`);
+
+        });
+    } catch (error) {
+        console.error('Failed to start server:', {
+            message: error.message,
+            stack: error.stack,
+            code: error.code
+        });
+        process.exit(1);
+    }
+};
+
+// Start server function call
+startServer();
