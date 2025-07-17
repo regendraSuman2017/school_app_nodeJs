59b4b09b3517eeba41c466fed25667cff286c1e5
Regendra Suman <regendrasuman2017@gmail.com>
2025-07-17 23:30:28 +0530

    Chnage DB3

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,24 +4,10 @@ import jwt from 'jsonwebtoken';
 
 export const checkEmail = async (req, res) => {
     try {
-        const { emailId } = req.body;
-
-        const existingUser = await db.query('SELECT * FROM auth_master');
-
-        if (existingUser.length > 0) {
-            return res.status(200).json({
-                success: true,
-                message: "Email exists",
-                data: {
-                    emailId,
-                    password: existingUser[0].password
-                }
-            });
-        }
-
-        return res.status(400).json({
-            success: false,
-            message: "Email does not exist"
+             return res.status(200).json({
+            success: true,
+            message: 'Succes',
+            error: error.message
         });
     } catch (error) {
         console.error('Error checking email:', error);
