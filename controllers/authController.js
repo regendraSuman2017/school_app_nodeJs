19b9b4eb2a8ca9ed6252af235d57a2e085cf2e89
Regendra Suman <116884018+regendraSuman2017@users.noreply.github.com>
2025-07-18 13:42:48 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,20 +11,23 @@ export const checkEmail = async (req, res) => {
             });
         }
 
-        const existingUser = await db.query('SELECT * FROM auth_master WHERE emailId = ?', [email]);
+        const existingUser = await db.query('INSERT INTO auth_master (emailId) VALUES (?)', [email]);
         
-        if (existingUser.length > 0) {
+        if (existingUser) {
             return res.status(200).json({
                 success: true,
                 message: 'Email ID is available'
             });
+        }else{
+          
+                return res.status(400).json({
+                    success: false,
+                    message: 'Email ID is not available'
+                });
+            
         }
 
 
-        return res.status(200).json({
-            success: true,
-            message: 'Email is available'
-        });
     } catch (error) {
         return res.status(500).json({
             success: false,
