dc0edcd38641a9e2b14e6dac375df3080c6a1fb9
Regendra Suman <116884018+regendraSuman2017@users.noreply.github.com>
2025-07-18 15:04:13 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,41 +1,9 @@
 import express from 'express';
-import authRoutes from './routes/authRoutes.js';
+import { checkEmail } from '../controllers/authController.js';
 
-const app = express();
+const router = express.Router();
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Authentication routes
+router.post('/checkEmail', checkEmail);
 
-// Routes
-app.use('/api/auth', authRoutes);
-
-app.use('/home', async (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'Welcome to the Home Page',
-        timestamp: new Date().toISOString()
-    });
-})
-
-// Handle undefined routes (404 Not Found)
-// Start the server
-const startServer = async () => {
-    try {
-        const port = 3000;
-        app.listen(port, () => {
-            console.log(` Server running at: http://localhost:${port}`);
-
-        });
-    } catch (error) {
-        console.error('Failed to start server:', {
-            message: error.message,
-            stack: error.stack,
-            code: error.code
-        });
-        process.exit(1);
-    }
-};
-
-// Start server function call
-startServer();
+export default router;
