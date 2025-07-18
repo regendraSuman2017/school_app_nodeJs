eafbfafe056c5c9fd1139200c0fe345349ab64d2
Regendra Suman <regendrasuman2017@gmail.com>
2025-07-18 23:00:59 +0530

    add class

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import sessionRoutes from './routes/sessionRoutes.js';
 import courseRoutes from './routes/courseRoutes.js';
+import classRoutes from './routes/classRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/session', sessionRoutes);
 app.use('/api/course', courseRoutes);
+app.use('/api/class', classRoutes);
 
 app.use('/home', async (req, res) => {
     res.json({
